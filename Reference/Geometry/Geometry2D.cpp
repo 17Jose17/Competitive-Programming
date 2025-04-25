87fa5c5f5093431782f6378fddffcaa847c7f159
Jose_17 <126434408+17Jose17@users.noreply.github.com>
2025-04-24 23:07:13 -0600

    Update Geometry2D.cpp

--- Reference/Geometry/Geometry2D.cpp
+++ Reference/Geometry/Geometry2D.cpp
@@ -31,8 +31,11 @@ struct point{
 	bool half(const point & p) const{return le(p.cross(*this), 0) || (eq(p.cross(*this), 0) && le(p.dot(*this), 0));}
 
 	bool left(const point & p, const point & q){ 
-        return (q - p).cross(*this - p) > eps;
-    }
+        	return (q - p).cross(*this - p) > eps;
+	}
+
+	ll cross(const point & p, const point & q) const{return (p - *this).cross(q - *this);}
+	ll dot(const point & p, const point & q) const{return (p - *this).dot(q - *this);}
     
 };
 
@@ -361,3 +364,222 @@ ld areaUnionTriangles(vector<vector<point>> & P){
     }
     return ans / 2;
 }
+
+int sgn(ll x) { return leq(x, 0) ? eq(x, 0) ? 0 : -1 : 1; }
+
+using pt = point;
+
+struct Edge {
+    pt l, r;
+};
+
+bool edge_cmp(Edge* edge1, Edge* edge2)
+{
+    const pt a = edge1->l, b = edge1->r;
+    const pt c = edge2->l, d = edge2->r;
+    int val = sgn(a.cross(b, c)) + sgn(a.cross(b, d));
+    if (val != 0)
+        return val > 0;
+    val = sgn(c.cross(d, a)) + sgn(c.cross(d, b));
+    return val < 0;
+}
+
+enum EventType { DEL = 2, ADD = 3, GET = 1, VERT = 0 };
+
+struct Event {
+    EventType type;
+    int pos;
+    bool operator<(const Event& event) const { return type < event.type; }
+};
+
+vector<Edge*> sweepline(vector<Edge*> planar, vector<pt> queries)
+{
+    using pt_type = decltype(pt::x);
+
+    auto s =
+        set<pt_type, std::function<bool(const pt_type&, const pt_type&)>>(le);
+    for (pt p : queries)
+        s.insert(p.x);
+    for (Edge* e : planar) {
+        s.insert(e->l.x);
+        s.insert(e->r.x);
+    }
+
+    int cid = 0;
+    auto id =
+        map<pt_type, int, std::function<bool(const pt_type&, const pt_type&)>>(
+            le);
+    for (auto x : s)
+        id[x] = cid++;
+
+    // create events
+    auto t = set<Edge*, decltype(*edge_cmp)>(edge_cmp);
+    auto vert_cmp = [](const pair<pt_type, int>& l,
+                       const pair<pt_type, int>& r) {
+        if (!eq(l.first, r.first))
+            return le(l.first, r.first);
+        return l.second < r.second;
+    };
+    auto vert = set<pair<pt_type, int>, decltype(vert_cmp)>(vert_cmp);
+    vector<vector<Event>> events(cid);
+    for (int i = 0; i < (int)queries.size(); i++) {
+        int x = id[queries[i].x];
+        events[x].push_back(Event{GET, i});
+    }
+    for (int i = 0; i < (int)planar.size(); i++) {
+        int lx = id[planar[i]->l.x], rx = id[planar[i]->r.x];
+        if (lx > rx) {
+            swap(lx, rx);
+            swap(planar[i]->l, planar[i]->r);
+        }
+        if (lx == rx) {
+            events[lx].push_back(Event{VERT, i});
+        } else {
+            events[lx].push_back(Event{ADD, i});
+            events[rx].push_back(Event{DEL, i});
+        }
+    }
+
+    vector<Edge*> ans(queries.size(), nullptr);
+    for (int x = 0; x < cid; x++) {
+        sort(events[x].begin(), events[x].end());
+        vert.clear();
+        for (Event event : events[x]) {
+            if (event.type == DEL) {
+                t.erase(planar[event.pos]);
+            }
+            if (event.type == VERT) {
+                vert.insert(make_pair(
+                    min(planar[event.pos]->l.y, planar[event.pos]->r.y),
+                    event.pos));
+            }
+            if (event.type == ADD) {
+                t.insert(planar[event.pos]);
+            }
+            if (event.type == GET) {
+                auto jt = vert.upper_bound(
+                    make_pair(queries[event.pos].y, planar.size()));
+                if (jt != vert.begin()) {
+                    --jt;
+                    int i = jt->second;
+                    if (geq(max(planar[i]->l.y, planar[i]->r.y),
+                           queries[event.pos].y)) {
+                        ans[event.pos] = planar[i];
+                        continue;
+                    }
+                }
+                Edge* e = new Edge;
+                e->l = e->r = queries[event.pos];
+                auto it = t.upper_bound(e);
+                if (it != t.begin())
+                    ans[event.pos] = *(--it);
+                delete e;
+            }
+        }
+
+        for (Event event : events[x]) {
+            if (event.type != GET)
+                continue;
+            if (ans[event.pos] != nullptr &&
+                eq(ans[event.pos]->l.x, ans[event.pos]->r.x))
+                continue;
+
+            Edge* e = new Edge;
+            e->l = e->r = queries[event.pos];
+            auto it = t.upper_bound(e);
+            delete e;
+            if (it == t.begin())
+                e = nullptr;
+            else
+                e = *(--it);
+            if (ans[event.pos] == nullptr) {
+                ans[event.pos] = e;
+                continue;
+            }
+            if (e == nullptr)
+                continue;
+            if (e == ans[event.pos])
+                continue;
+            if (id[ans[event.pos]->r.x] == x) {
+                if (id[e->l.x] == x) {
+                    if (ge(e->l.y, ans[event.pos]->r.y))
+                        ans[event.pos] = e;
+                }
+            } else {
+                ans[event.pos] = e;
+            }
+        }
+    }
+    return ans;
+}
+
+struct DCEL {
+    struct Edge {
+        pt origin;
+        Edge* nxt = nullptr;
+        Edge* twin = nullptr;
+        int face;
+    };
+    vector<Edge*> body;
+};
+
+vector<pair<int, int>> point_location(DCEL planar, vector<pt> queries)
+{
+    vector<pair<int, int>> ans(queries.size());
+    vector<Edge*> planar2;
+    map<intptr_t, int> pos;
+    map<intptr_t, int> added_on;
+    int n = planar.body.size();
+    for (int i = 0; i < n; i++) {
+        if (planar.body[i]->face > planar.body[i]->twin->face)
+            continue;
+        Edge* e = new Edge;
+        e->l = planar.body[i]->origin;
+        e->r = planar.body[i]->twin->origin;
+        added_on[(intptr_t)e] = i;
+        pos[(intptr_t)e] =
+            le(planar.body[i]->origin.x, planar.body[i]->twin->origin.x)
+                ? planar.body[i]->face
+                : planar.body[i]->twin->face;
+        planar2.push_back(e);
+    }
+    auto res = sweepline(planar2, queries);
+    for (int i = 0; i < (int)queries.size(); i++) {
+        if (res[i] == nullptr) {
+            ans[i] = make_pair(1, -1);
+            continue;
+        }
+        pt p = queries[i];
+        pt l = res[i]->l, r = res[i]->r;
+        if (eq(p.cross(l, r), 0) && leq(p.dot(l, r), 0)) {
+            ans[i] = make_pair(0, added_on[(intptr_t)res[i]]);
+            continue;
+        }
+        ans[i] = make_pair(1, pos[(intptr_t)res[i]]);
+    }
+    for (auto e : planar2)
+        delete e;
+    return ans;
+}
+
+vector<DCEL::Edge*> edges(k), twins(k);
+for(int j = 0; j < k; j++){
+    edges[j] = new DCEL::Edge{v[j]};
+    twins[j] = new DCEL::Edge{v[(j + 1) % k]};
+    edges[j]->twin = twins[j];
+    twins[j]->twin = edges[j];
+    edges[j]->face = i;
+    twins[j]->face = -1;
+}
+
+for(int j = 0; j < k; j++){
+    edges[j]->nxt = edges[(j + 1) % k];
+    twins[j]->nxt = twins[(j - 1 + k) % k];
+}
+
+for(int j = 0; j < k; j++){
+    dcel.body.push_back(edges[j]);
+    dcel.body.push_back(twins[j]);
+}
+
+auto res = point_location(dcel, ps);
